7e953737bb0e494bd71a2905b46334e3ceda9971
luis3977@gmail.com <luis3977@gmail.com>
2021-01-21 21:39:52 -0200

    Created DTO

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,24 +1,25 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
-	public List<Category> findAll() {
-		
-		List<Category> list = this.categoryRepository.findAll();
-		
-		return list;
-	}
 
+	public List<CategoryDTO> findAll() {
+
+		List<Category> list = this.categoryRepository.findAll();
+
+		return list.stream().map(entity -> new  CategoryDTO(entity)).collect(Collectors.toList());
+	}
 }
