a0c40fbc9728bf5679e59225457d5b67242804be
luis3977@gmail.com <luis3977@gmail.com>
2021-02-16 22:54:34 -0200

    Add button, image and link catalog page.

--- front-end/src/core/components/Navbar/index.tsx
+++ front-end/src/core/components/Navbar/index.tsx
@@ -12,7 +12,7 @@ const Navbar = () => (
         <div className="col-6 offset-2">
             <ul className="main-menu">
                 <li>
-                   <NavLink to="link" activeClassName="active" exact>
+                   <NavLink to="/" activeClassName="active" exact>
                       HOME
                    </NavLink>
                 </li>
